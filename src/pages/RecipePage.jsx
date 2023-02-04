96b7dc702a1aef8883066a4fca525020716f1a43
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2023-02-04 20:23:00 +0530

    added styling to recipe page

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -3,11 +3,21 @@ import { GlobalContext } from "../context/globalContext";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase.config";
 import { collection, onSnapshot } from "firebase/firestore";
+import FoodPattern from '../assets/ingredient_pattern.jpg'
 import {
   RecipePageContainer,
-  LeftSideContainer,
-  RightSideContainer,
+  BannerImageContainer,
+  RecipeInfoContainer,
+  RecipeDesc,
+  RecipeStepsContainer,
+  RecipeLeftSideContainer,
+  RecipeRightSideContainer,
+  IngredientCard,
+  IngredientCardbg,
+  IngredientList,
+  IngredientListContainer
 } from "../styles/recipePage.style";
+import { Typography } from "@mui/material";
 
 const RecipePage = () => {
   const { loginUserState } = useContext(GlobalContext);
@@ -41,11 +51,47 @@ const RecipePage = () => {
 
   return (
     <RecipePageContainer>
-      <LeftSideContainer background={currentRecipe?.bg}>
-      </LeftSideContainer>
-      <RightSideContainer>
-        <h1>{currentRecipe?.title}</h1>
-      </RightSideContainer>
+      <BannerImageContainer background={currentRecipe?.bg} />
+      
+      <RecipeInfoContainer>
+       <RecipeLeftSideContainer>
+       <h2>Description</h2>
+        <RecipeDesc>
+            {currentRecipe?.desc}
+        </RecipeDesc>
+        <h2>Steps to follow</h2>
+        <RecipeStepsContainer>
+            {currentRecipe?.steps?.map((step, index) => {
+                return (
+                    <div key={index}>
+                        
+                        <p variant='h6'>{index + 1}.  {step}</p>
+                    </div>
+                )
+            })}
+        </RecipeStepsContainer>
+       </RecipeLeftSideContainer>
+       <RecipeRightSideContainer>
+         <IngredientCard>
+            <IngredientCardbg background={FoodPattern}>
+         <h2>Ingredients</h2>
+                
+            </IngredientCardbg>
+            <IngredientListContainer>
+
+            <IngredientList>
+                {currentRecipe?.ingredients?.map((ingredient, index) => {
+                    return (
+                        <li key={index}>
+                            <span variant='h6'>{ingredient}</span>
+                        </li>
+                    )
+                })}
+                </IngredientList>
+            </IngredientListContainer>
+         </IngredientCard>
+       </RecipeRightSideContainer>
+      </RecipeInfoContainer>
     </RecipePageContainer>
   );
 };
--- src/styles/recipePage.style.js
+++ src/styles/recipePage.style.js
@@ -7,8 +7,8 @@ export const RecipePageContainer = styled.div`
   flex-direction: column;
 `;
 
-export const LeftSideContainer = styled.div`
-  flex: 0.6;
+export const BannerImageContainer = styled.div`
+  flex: 0.7;
   
   background-position: center center;
   background-repeat: no-repeat;
@@ -17,7 +17,84 @@ export const LeftSideContainer = styled.div`
     url(${(props) => props.background});
     
 `;
-export const RightSideContainer = styled.div`
-  flex: 0.4;
-  padding: 10px 50px;
+export const RecipeInfoContainer = styled.div`
+  flex: 0.3;
+margin: 20px auto;
+  /* padding: 10px 250px; */
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  
+
+  h2{
+    font-size: 29px;
+    margin-bottom: 10px;
+  }
 `;
+
+
+export const RecipeLeftSideContainer = styled.div`
+    flex: 0.5;
+    width: 59%;
+    padding: 10px 30px;
+    `
+export const RecipeRightSideContainer = styled.div`
+padding: 10px 30px;
+/* padding: 10px 100px; */
+
+flex: 0.5;
+
+`
+
+export const RecipeDesc = styled.p`
+    /* width: 40%; */
+    font-size: 17px;
+  margin-bottom: 20px;
+`
+
+export const RecipeStepsContainer = styled.div`
+    /* width: 40%; */
+
+    p{
+        margin-bottom: 10px;
+        font-size: 17px;
+    }
+`
+export const IngredientCard = styled.div`
+box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+border-radius: 10px;
+/* padding: 10px 20px; */
+    display: flex;
+    flex-direction: column;
+
+    h2{
+        color: #fff;
+        font-weight: 600;
+    }
+    `
+
+export const IngredientListContainer = styled.div`
+padding: 10px 20px;
+flex: 0.7;
+
+`
+
+export const IngredientCardbg = styled.div`
+    background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.6)),
+    url(${(props) => props.background});
+    flex: 0.3;
+    padding: 15px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    `
+export const IngredientList = styled.ul`
+    list-style: none;
+    padding: 10px 20px;
+
+    li{
+        margin-bottom: 6px;
+    }
+`
