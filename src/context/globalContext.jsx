bc1983d7d0f6a02d43ea6a334a253881229b74b2
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2023-02-04 13:05:31 +0530

    update firebase.config

--- src/context/globalContext.jsx
+++ src/context/globalContext.jsx
@@ -1,6 +1,6 @@
 import React,{ createContext, useState,  useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { db } from "../firebase.config";
 
 export const GlobalContext = React.createContext();
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,47 @@
-import React,{useContext} from 'react'
+import React,{useState,useEffect,useContext} from 'react'
 import { GlobalContext } from '../context/globalContext'
+import { db } from '../firebase.config'
+import {collection,onSnapshot} from 'firebase/firestore'
+
 
 const Home = () => {
     const {loginUserState, setLoginUserState ,handleLogout} = useContext(GlobalContext)
+
+    const [recipes, setRecipes] = useState([])
+    const [form, setForm] = useState({
+        title:'',
+        desc:'',
+        images:[],
+        ingredients:[],
+        steps:[]
+    })
+    const [popupActive, setPopupActive] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const recipesCollectionRef = collection(db, 'recipes')
+
+    useEffect(() => {
+        onSnapshot(recipesCollectionRef, (snapshot) => {
+            const data = snapshot.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data()
+                }
+            })
+            setRecipes(data)
+            setLoading(false)
+        }) 
+        
+    }, [])
+
+    console.log(recipes, 'recipes')
 
   return (
     <div>
         welcome to home
         <button onClick={() =>handleLogout()}>logout</button>
+        <div>
+
+        </div>
     </div>
   )
 }
